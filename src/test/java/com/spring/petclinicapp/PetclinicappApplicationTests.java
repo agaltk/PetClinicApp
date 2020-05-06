3051df7ec3f806f84d7c80de63096228a3ccd284
Aga <aga.ltk@gmail.com>
2020-05-06 22:48:01 +0200

    init project

---
+++ src/test/java/com/spring/petclinicapp/PetclinicappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.petclinicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
