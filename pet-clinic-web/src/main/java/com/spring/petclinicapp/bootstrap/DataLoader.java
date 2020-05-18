93361e29182d6bff4c506f7fe0a67ac59e17ed39
Aga <aga.ltk@gmail.com>
2020-05-18 22:44:46 +0200

    auto generated id added

--- pet-clinic-web/src/main/java/com/spring/petclinicapp/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/petclinicapp/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Lisa");
         owner1.setLastName("Smith");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("John");
         owner2.setLastName("Snow");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loaded...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Kim");
         vet1.setLastName("Picard");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Mary");
         vet2.setLastName("Jane");
 
