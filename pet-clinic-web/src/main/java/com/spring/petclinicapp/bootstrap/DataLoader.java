0c5ef8847b6b03268323d35b01966d4d1b4f9aac
Aga <aga.ltk@gmail.com>
2020-05-20 23:13:43 +0200

    added pets and contact info in bootloader, modified owner service

--- pet-clinic-web/src/main/java/com/spring/petclinicapp/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/petclinicapp/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.spring.petclinicapp.bootstrap;
 
 import com.spring.petclinicapp.model.Owner;
+import com.spring.petclinicapp.model.Pet;
 import com.spring.petclinicapp.model.PetType;
 import com.spring.petclinicapp.model.Vet;
 import com.spring.petclinicapp.services.OwnerService;
@@ -8,6 +9,8 @@ import com.spring.petclinicapp.services.PetTypeService;
 import com.spring.petclinicapp.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,13 +41,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Lisa");
         owner1.setLastName("Smith");
+        owner1.setAddress("long street 123");
+        owner1.setCity("London");
+        owner1.setTelephone("1233212");
 
+        Pet lisaPet  = new Pet();
+        lisaPet.setPetType(dog);
+        lisaPet.setName("Miki");
+        lisaPet.setBirthDate(LocalDate.now());
+        lisaPet.setOwner(owner1);
+
+        owner1.getPets().add(lisaPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("John");
         owner2.setLastName("Snow");
+        owner2.setAddress("short street 44");
+        owner2.setCity("New York");
+        owner2.setTelephone("6555");
 
+
+        Pet johnCat  = new Pet();
+        johnCat.setPetType(cat);
+        johnCat.setName("Sim");
+        johnCat.setBirthDate(LocalDate.now());
+        johnCat.setOwner(owner2);
+        owner2.getPets().add(johnCat);
         ownerService.save(owner2);
 
         System.out.println("Owners loaded...");
