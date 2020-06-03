4f66a707ac32165145626404e6cfcd1a67108c7a
Aga <aga.ltk@gmail.com>
2020-06-03 13:07:07 +0200

    web binder added to controller

--- pet-clinic-web/src/main/java/com/spring/petclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/spring/petclinicapp/controllers/VetController.java
@@ -3,6 +3,8 @@ package com.spring.petclinicapp.controllers;
 import com.spring.petclinicapp.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +16,10 @@ public class VetController {
         this.vetService = vetService;
     }
 
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
     @RequestMapping({"/vets", "vets/index", "vets/index.html", "vets.html"})
     public String listVets(Model model){
 
